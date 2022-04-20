d9ca15c041a4450f07c6260e7451d31dadb2108e
Tomás Sánchez Martínez <tomassanchezmartinez@MacBook-Pro-de-Tomas.local>
2022-04-20 21:11:38 +0200

    Added MainBoxComponent.js

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+import MainBoxComponent from "./components/MainBoxComponent/MainBoxComponent.js";
+
+const mainBox = document.querySelector(".main-container");
+
+// eslint-disable-next-line no-new
+new MainBoxComponent(mainBox);
