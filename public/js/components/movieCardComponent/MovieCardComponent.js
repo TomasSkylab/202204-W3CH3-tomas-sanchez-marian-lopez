9f98cabdd4064c82e801bf4ec9791e92ee76f854
Marian <marianlopig@gmail.com>
2022-04-21 01:50:02 +0200

    rating component created

--- public/js/components/movieCardComponent/MovieCardComponent.js
+++ public/js/components/movieCardComponent/MovieCardComponent.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-new */
+import RatingComponent from "../../ratingComponent/RatingComponent.js";
 import Component from "../Component.js";
 
 class MovieCardComponent extends Component {
@@ -13,26 +15,12 @@ class MovieCardComponent extends Component {
                 />
                 <h4 class="serie__title">${movie.name}</h4>
                 <p class="serie__info">${movie.creator} (${movie.year})</p>
-                <ul class="score">
-                  <li class="score__star">
-                    <i class="icon--score fas fa-star" title="1/5"></i>
-                  </li>
-                  <li class="score__star">
-                    <i class="icon--score fas fa-star" title="2/5"></i>
-                  </li>
-                  <li class="score__star">
-                    <i class="icon--score fas fa-star" title="3/5"></i>
-                  </li>
-                  <li class="score__star">
-                    <i class="icon--score fas fa-star" title="4/5"></i>
-                  </li>
-                  <li class="score__star">
-                    <i class="icon--score fas fa-star" title="5/5"></i>
-                  </li>
-                </ul>
-                <i class="fas fa-times-circle icon--delete"></i>
-              
+                <div class="rating">
+                </div>
+                <i class="fas fa-times-circle icon--delete"></i>              
     `;
+    const parentRating = this.element.querySelector("div.rating");
+    new RatingComponent(parentRating, movie);
   }
 }
 
