f14b364e6574cad59cd1e074fcf75ad28e746a71
Tomás Sánchez Martínez <tomassanchezmartinez@MacBook-Pro-de-Tomas.local>
2022-04-20 19:22:01 +0200

    Edited eslint again

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,16 @@
 module.exports = {
   env: {
-    browser: true,
-
     es2021: true,
-
     node: true,
-
+    browser: true,
     jest: true,
   },
-
   extends: ["airbnb-base", "prettier"],
-
   parserOptions: {
     ecmaVersion: "latest",
-
     sourceType: "module",
   },
-
   rules: {
-    "import/extensions": "off",
     "no-plusplus": "off",
     "no-restricted-syntax": "off",
     "lines-between-class-members": [
@@ -26,16 +18,6 @@ module.exports = {
       "always",
       { exceptAfterSingleLine: true },
     ],
-  },
-  babel: {
-    env: {
-      test: {
-        plugins: ["@babel/plugin-transform-modules-commonjs"],
-      },
-    },
-  },
-
-  jest: {
-    testEnvironment: "jsdom",
+    "import/extensions": "off",
   },
 };
