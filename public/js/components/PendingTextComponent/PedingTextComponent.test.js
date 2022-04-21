54c933851596ddf4f061de78f2e329d9b4aa95d3
Tomás Sánchez Martínez <tomassanchezmartinez@MacBook-Pro-de-Tomas.local>
2022-04-21 19:37:08 +0200

    Tested PendingTextCommponent.js

--- public/js/components/PendingTextComponent/PedingTextComponent.test.js
+++ public/js/components/PendingTextComponent/PedingTextComponent.test.js
@@ -1,15 +1,33 @@
 import PendingTextComponent from "./PendingTextComponent.js";
 
 describe("Given a PendingTextComponent component", () => {
-  describe("When it is instantiated", () => {
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+  });
+
+  describe("When instantiated", () => {
     test("Then it should create a p element", () => {
-      const container = document.createElement("div");
       const newParagraph = new PendingTextComponent(container, "", "");
       const expectedElementType = "P";
 
       const newElementType = newParagraph.element.nodeName;
 
       expect(newElementType).toBe(expectedElementType);
+    });
+  });
+
+  describe("When it`s recieved a 'You have 4 series pending to watch'", () => {
+    test("Then it should show 'You have 4 series pending to watch'", () => {
+      const text = "You have 4 series pending to watch";
+      const expectedText = '<p class="You have 4 series pending to watch"></p>';
+
+      new PendingTextComponent(container, text);
+
+      const testElement = container.innerHTML;
+
+      expect(testElement).toBe(expectedText);
     });
   });
 });
