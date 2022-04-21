46d3e6b73f6266ed54aa8a353f05e323814ab367
Marian <marianlopig@gmail.com>
2022-04-21 23:55:54 +0200

    rating stars colours added and general code refactor

--- public/js/components/movieCardComponent/MovieCardComponent.js
+++ public/js/components/movieCardComponent/MovieCardComponent.js
@@ -7,20 +7,28 @@ class MovieCardComponent extends Component {
   constructor(parentElement, movie) {
     super("li", "serie", parentElement);
     this.movie = movie;
+    this.render();
+  }
+
+  render() {
     this.element.innerHTML = `
                 <img
                   class="serie__poster"
-                  src="${movie.poster}"
-                  alt="${movie.name} poster"
+                  src="${this.movie.poster}"
+                  alt="${this.movie.name} poster"
                 />
-                <h4 class="serie__title">${movie.name}</h4>
-                <p class="serie__info">${movie.creator} (${movie.year})</p>
+                <h4 class="serie__title">${this.movie.name}</h4>
+                <p class="serie__info">${this.movie.creator} (${this.movie.year})</p>
                 <div class="rating">
                 </div>
                 <i class="fas fa-times-circle icon--delete"></i>              
     `;
     const parentRating = this.element.querySelector("div.rating");
-    new RatingComponent(parentRating, movie);
+    new RatingComponent(parentRating, this.movie.score, this.onRating);
+  }
+
+  onRating(score) {
+    this.movie = { ...this.movie, score };
   }
 }
 
